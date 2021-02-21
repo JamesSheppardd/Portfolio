275cb8da6fed679ee3476725c60471c47c3456bb
JamesSheppardd <70041352+JamesSheppardd@users.noreply.github.com>
2021-02-21 12:54:28 +0000

    Bug fix with weather app image 3

--- src/Components/Common/assets/text-files/projects/current-weather-app/current-weather-app.jsx
+++ src/Components/Common/assets/text-files/projects/current-weather-app/current-weather-app.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import img1 from "./photo1.png";
 import img2 from "./photo2.png";
-import img3 from "./photo3.png";
+import img3 from "./photo3.PNG";
 
 const StyledDiv = styled.div`
     font-size: 23px;
